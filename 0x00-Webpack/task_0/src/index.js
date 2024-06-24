4da431531cfc7ff36b78c5e5b986b70aa5096e28
DouibaOmar <douibaomar40@gmail.com>
2024-06-24 17:07:54 +0100

    Done

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
 import $ from "jquery";
 
-$(document).ready(function () {
-  $("body").append("Holberton Dashboard");
-  $("body").append("Dashboard data for the students");
-  $("body").append("Copyright - Holberton School");
-});
+$("body").append("Holberton Dashboard");
+$("body").append("Dashboard data for the students");
+$("body").append("Copyright - Holberton School");
