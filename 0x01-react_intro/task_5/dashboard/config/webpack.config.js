0850a227265d17f41ab26b45a17e14615b44dfc9
DouibaOmar <douibaomar40@gmail.com>
2024-06-29 11:56:31 +0100

    Done

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,57 +1,51 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: "bundle.js",
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    hot: true,
   },
   mode: "development",
+  devtool: "inline-source-map",
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(gif|png|jpg|jpeg|ico)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
-          "file-loader",
+          {
+            loader: "file-loader",
+          },
           {
             loader: "image-webpack-loader",
             options: {
-              bypassOnDebug: true,
-              disable: true,
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
       },
     ],
   },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    open: true,
-    port: 8546,
-    hot: true,
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./dist/index.html",
-      filename: "index.html",
-      inject: false,
-    }),
-  ],
 };
